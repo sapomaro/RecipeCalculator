4a7aa89958129b97575c5136e062b7c02e430351
Optimizitor <sapomaro@gmail.com>
2023-01-21 00:26:54 +0300

    feat(RecipeCalculator): add components and text

--- src/RecipeCalculator.jsx
+++ src/RecipeCalculator.jsx
@@ -1,8 +1,48 @@
+import { StoreProvider as LocalStoreProvider } from './store';
+import { Note, RadioField, Textarea } from './components';
+
 export function RecipeCalculator() {
   return (
-    <div>
-      <h1>Калькулятор ингредиентов</h1>
+    <LocalStoreProvider>
+      <div>
+        <h1>Калькулятор ингредиентов и форм для выпечки</h1>
 
-    </div>
+        <h2>Для чего он нужен и как работает</h2>
+
+        <p>Вы нашли рецепт, который рассчитан на большое количество порций, 
+          а вы хотите приготовить в два раза меньше. Либо в инструкции указана 
+          большая круглая форма для выпечки, а у вас есть только маленькая 
+          квадратная, и нужно пересчитать количество ингредиентов, чтобы торт 
+          получился такой же высоты. Для всех подобных случаев и создан этот 
+          кулинарный калькулятор.
+        </p>
+
+        <h2>Шаг №1: укажите исходные данные из рецепта</h2>
+
+        <h3>Количество порций ∕ размеры формы для выпечки:</h3>
+
+        <RadioField name="servings" group="in" />
+        <RadioField name="round_pan" group="in" />
+        <RadioField name="rect_pan" group="in" />
+
+        <h3>Ингредиенты:</h3>
+
+        <Textarea name="ingredients" group="in" />
+
+        <h2>Шаг №2: укажите желаемые параметры для пересчёта</h2>
+
+        <h3>Новое количество порций ∕ размеры вашей формы для выпечки:</h3>
+
+        <RadioField name="servings" group="out" />
+        <RadioField name="round_pan" group="out" />
+        <RadioField name="rect_pan" group="out" />
+
+        <h3>Результат рассчётов:</h3>
+
+        <Textarea name="ingredients" group="out" />
+
+        <Note name="summary" group="out" />
+      </div>
+    </LocalStoreProvider>
   );
 }
