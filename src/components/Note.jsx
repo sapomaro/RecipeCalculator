7d9d61a9a2eb55ce28e96bb139989a8e854d9a0b
Optimizitor <sapomaro@gmail.com>
2023-01-21 21:31:31 +0300

    feat: implement Note

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,40 @@
-import { useStoreElementState } from '../store';
+import { aggregateDataSelector, useStoreSelector } from '../store';
 
-export function Note({ name, group }) {
-  const state = useStoreElementState({ name, group });
+import './Note.scss';
+
+export function Note() {
+  const data = useStoreSelector(aggregateDataSelector);
+
+  const { inputShape, inputSize, multiplier, outputShape, outputSize } = data;
+
+	const getSizeInfo = (shape, size) => {
+		if (!shape || !size) {
+      return '';
+    }
+
+		switch (shape) {
+			case 'servings':
+        return `порции, ${size} ед.`;
+			case 'round':
+        return `круглая форма, ${size} см в диаметре`;
+			case 'rectangle':
+        return `прямоугольная форма, ${size} см по длине и ширине`;
+      case 'square': 
+				return `квадратная форма, ${size} см по длине и ширине`;
+		}
+	};
 
   return (
-    <p>{state.value}</p>
+		<div className="note">
+      <p>{multiplier ? 'Вес ∕ объём каждного ингредиента перемножается на: ' + multiplier + '' : ''}</p>
+
+      <p>Исходные данные из рецепта: <b>{getSizeInfo(inputShape, inputSize) || 'укажите выше ⬆'}</b></p>
+
+      <p>Желаемые параметры для пересчёта: <b>{getSizeInfo(outputShape, outputSize) || 'укажите выше ⬆'}</b></p>
+
+			<p>Рекомендуем перепроверять полученные данные, поскольку в редких случаях алгоритм 
+        может не обработать их должным образом, в частности когда исходный текст рецепта 
+        имеет нестандартное форматирование.</p>
+		</div>
   );
 };
